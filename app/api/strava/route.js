e51023ea6747e9de23b169da27e50a8ed71b6bd8
Tobias Fröhlich <froehlich_tobias@icloud.com>
2025-02-25 19:54:36 +0100

    added some metadata

--- app/api/strava/route.js
+++ app/api/strava/route.js
@@ -1,3 +1,17 @@
-export function GET() {
-  return Response.json({ message: "You just hit an API 😂!" });
+export async function GET() {
+  const res = await fetch(
+    "https://strava.com/api/v3/athlete/activities?access_token=0c11745583920314672e5b6efc10526ab5395b69"
+  );
+  const activities = await res.json();
+
+  // Extract only the required fields
+  const filteredData = activities.map(
+    ({ elapsed_time, sport_type, start_date }) => ({
+      elapsed_time,
+      sport_type,
+      start_date,
+    })
+  );
+
+  return Response.json(filteredData);
 }
--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,21 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Tobias Fröhlich",
-  description: "personal website",
+  title: "Tobias Fröhlich - Personal Website",
+  description:
+    "Welcome to my personal website. I am a tech enthusiast with a passion for learning and curiosity about everything, based in Austria.",
+  keywords:
+    "Tobias Fröhlich, software engineer, web developer, Next.js, React, JavaScript, AI projects, automation, portfolio, CV, tech enthusiast, language learning, freelance",
+  author: "Tobias Fröhlich",
+  openGraph: {
+    title: "Tobias Fröhlich - Software Engineer & Developer Portfolio",
+    description:
+      "Explore my software projects, CV, and personal interests in tech, AI, language learning, and fitness.",
+    url: "https://tobiasfrohlich.com", // Replace with your actual website URL
+    siteName: "Tobias Fröhlich",
+
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
