546d4541e15530579a78f380f5929eb24d6d48f6
Tobias Fröhlich <froehlich_tobias@icloud.com>
2025-02-25 20:01:21 +0100

    changed metadata

--- app/layout.js
+++ app/layout.js
@@ -12,19 +12,17 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Tobias Fröhlich - Personal Website",
+  title: "Tobias Fröhlich - Personal Website and Portfolio",
   description:
-    "Welcome to my personal website. I am a tech enthusiast with a passion for learning and curiosity about everything, based in Austria.",
+    "Explore my software projects, CV, and personal interests in tech, AI, language learning, and endurance training.",
   keywords:
-    "Tobias Fröhlich, software engineer, web developer, Next.js, React, JavaScript, AI projects, automation, portfolio, CV, tech enthusiast, language learning, freelance",
-  author: "Tobias Fröhlich",
+    "Tobias Fröhlich, software engineer, web developer, Next.js, React, JavaScript, AI projects, automation, portfolio, CV, tech enthusiast, language learning, freelance, fitness",
   openGraph: {
     title: "Tobias Fröhlich - Software Engineer & Developer Portfolio",
     description:
       "Explore my software projects, CV, and personal interests in tech, AI, language learning, and fitness.",
-    url: "https://tobiasfrohlich.com", // Replace with your actual website URL
-    siteName: "Tobias Fröhlich",
-
+    url: "https://tobiasfrohlich.com",
+    siteName: "Tobias Fröhlich - Personal Website",
     type: "website",
   },
 };
